4ecab51c7280f6d1142cb0cd8feceaa959684868
Gopinath <Gopinath.b@axcrypt.net>
2026-09-01 14:04:41 +0530

    Virtual Drive Framework Implementation

---
+++ shared/AxCrypt.Core/Runtime/VirtualFileWorkFolder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static AxCrypt.Abstractions.TypeResolve;
+
+namespace AxCrypt.Core.Runtime
+{
+    public class VirtualFileWorkFolder : FileWorkFolder
+    {
+
+        private static string resolvePath(string fallbackPath)
+        {
+            string retPath = fallbackPath;
+            try
+            {
+                retPath = New<IInMemoryFileSystem>().Storage + "\\";
+            }
+            catch { }
+            return retPath;
+        }
+        public VirtualFileWorkFolder(string path) : base(resolvePath(path))
+        {
+
+        }
+    }
+}
